9739ac8327cfca4d22610fbe93e35049991c8305
Lukas Lampl <lukas.lampl@gmx.net>
2024-12-31 10:02:04 +0100

    FIXED typo.

--- app/src/main/java/app/utils/MacroBlock.java
+++ app/src/main/java/app/utils/MacroBlock.java
@@ -494,7 +494,7 @@ public class MacroBlock implements Discardable {
 	 * 
 	 * @return The x position of the MacroBlock.
 	 */
-	public int getPositonX() {
+	public int getPositionX() {
 		return this.positionX;
 	}
 	
